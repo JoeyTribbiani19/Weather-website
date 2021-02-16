284cc597aee685cfeed1eec85e56e955bc7a7e36
Rahul Kotharu <rahulkotharu@rahuls-mbp.myfiosgateway.com>
2021-02-16 13:11:45 -0500

    init commit

---
+++ src/app.js
@@ -0,0 +1,106 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const { response } = require('express')
+const geocode = require('./utils/geocode')
+const forcast = require('./utils/forecast')
+
+const app = express()
+
+//define paths
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, 'templates/views')
+const partialsPath = path.join(__dirname, 'templates/partials')
+console.log(__dirname)
+
+//setup handle bars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+//setup static directory
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Joey'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Joey'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is some helpful text.',
+        title: 'Help',
+        name: 'Joey'
+    })
+})
+
+app.get('/products', (req, res) => {
+
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
+    console.log(req.query.search)
+    res.send({
+        products: []
+    }
+
+    )
+})
+
+app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'please provide a valid address'
+        })
+
+    }
+    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return res.send({ error })
+        }
+        forcast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error })
+            }
+            res.send({
+                forcast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
+    })
+    // res.send({
+    //     forecast: 'It is snowing',
+    //     location: 'Philadelphia',
+    //     address: req.query.address
+    // })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404 Help',
+        name: 'Joeys help',
+        errorMessage: 'Help page not found'
+    })
+})
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Joey',
+        errorMessage: 'page not Found'
+    })
+})
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.')
+})
